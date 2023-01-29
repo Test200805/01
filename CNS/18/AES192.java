739f449c571a32cb6694e0fe867176bdf9e8117f
Test200805 <123650320+Test200805@users.noreply.github.com>
2023-01-29 20:51:34 +0530

    Update AES192.java

--- CNS/18/AES192.java
+++ CNS/18/AES192.java
@@ -10,10 +10,7 @@ import java.util.Scanner;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-/**
- *
- * @author goushik
- */
+
 public class AES192 
 {
     public static String KEY = "password";
