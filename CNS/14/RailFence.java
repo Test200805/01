655f0c390f8399fd0299c13f317488225db0d49c
Test200805 <123650320+Test200805@users.noreply.github.com>
2023-01-29 20:50:11 +0530

    Update RailFence.java

--- CNS/14/RailFence.java
+++ CNS/14/RailFence.java
@@ -1,14 +1,6 @@
 package CNS;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author goushik
- */
 import java.util.*;
 public class RailFence 
 {    
